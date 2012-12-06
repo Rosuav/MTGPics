b39a197860655384b34f92fccd371a531ea71fbb
Chris Angelico <rosuav@gmail.com>
2012-12-06 17:28:33 +1100

    Initial commit with all art up to 20121205.

---
+++ Places to look.cmd
@@ -0,0 +1,13 @@
+rem Where does MTGO store its pics?
+
+rem Home screen, store background, and something else:
+start "" "C:\Program Files\Wizards of the Coast\Magic Online\Graphics\UI200610\Backgrounds"
+pause
+rem Login screen:
+start "" "C:\Program Files\Wizards of the Coast\Magic Online\Graphics\UI\backgrounds\scene"
+pause
+rem Loader background:
+start "" "C:\Program Files\Wizards of the Coast\Magic Online\Graphics\Loader"
+pause
+rem Kicker background:
+start "" "C:\Program Files\Wizards of the Coast\Magic Online\Graphics\Logos"
